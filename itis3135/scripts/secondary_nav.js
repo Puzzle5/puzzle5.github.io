320e9ad2ecc51d8dfc6333fe7378309ba4bca573
Puzzle5 <pchapin@charlotte.edu>
2025-03-25 07:18:49 +0900

    Updated secondary nav (debugging)

--- itis3135/scripts/secondary_nav.js
+++ itis3135/scripts/secondary_nav.js
@@ -1,15 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projectsMenu = document.querySelector('.projects-menu');
-    const projectsNav = document.querySelector('.projects-nav');
+const projectsMenu = document.querySelector('.projects-menu');
+const projectsNav = document.querySelector('.projects-nav');
 
-    // *Important*:  Hide the element *before* attaching event listeners.
+projectsMenu.addEventListener('mouseenter', () => {
+    projectsNav.style.display = 'flex';
+});
+
+projectsMenu.addEventListener('mouseleave', () => {
     projectsNav.style.display = 'none';
+});
 
-    projectsMenu.addEventListener('mouseenter', () => {
-        projectsNav.style.display = 'flex';
-    });
+projectsNav.addEventListener('mouseenter', () => {
+    projectsNav.style.display = 'flex';
+});
 
-    projectsMenu.addEventListener('mouseleave', () => {
-        projectsNav.style.display = 'none';
-    });
+projectsNav.addEventListener('mouseleave', () => {
+    projectsNav.style.display = 'none';
 });
