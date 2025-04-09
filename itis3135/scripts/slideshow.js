db460f1f0c868818f679e9cd79706e2a8bb401fd
Puzzle5 <pchapin@charlotte.edu>
2025-04-09 10:35:24 +0900

    Slideshow: Adjusting controls

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -2,6 +2,7 @@
 let slideIndex = 0;
 let slides = $(".mySlides");
 let listItems = $(".slide-list li");
+let intervalId; // To store the interval ID for auto-play
 
 function showSlide(n) {
 	slides.hide();
@@ -26,10 +27,71 @@ showSlide(slideIndex);
 listItems.on("click", function() {
 	let slideNumber = $(this).data("slide");
 	currentSlide(slideNumber);
+	stopAutoPlay(); // Stop auto-play when user clicks a list item
+});
+
+// Function for automatic slideshow
+function autoPlay() {
+	intervalId = setInterval(() => {
+		slideIndex++;
+		showSlide(slideIndex);
+	}, 5000);
+}
+
+// Function to stop automatic slideshow
+function stopAutoPlay() {
+	clearInterval(intervalId);
+}
+
+// Event listener for the start button
+$("#start").on("click", function() {
+	autoPlay(); // Start auto-play
+});
+
+// Event listener for the stop button
+$("#stop").on("click", function() {
+	stopAutoPlay(); // Stop auto-play
+});
+
+// Event listener for the forward button
+$("#forward").on("click", function() {
+	slideIndex++;
+	showSlide(slideIndex);
+	stopAutoPlay();
+});
+
+// Event listener for the back button
+$("#backwards").on("click", function() {
+	slideIndex--;
+	showSlide(slideIndex);
+	stopAutoPlay();
+});
+
+// Event listener for the beginning button
+$("#beginning").on("click", function() {
+	showSlide(slideIndex = 0);
+	stopAutoPlay();
+});
+
+// Event listener for the end button
+$("#end").on("click", function() {
+	showSlide(slideIndex = slides.length - 1);
+	stopAutoPlay();
+});
+
+// Event listener for fullscreen
+$("#fullscreen").on("click", function() {
+	let container = $(".slideshow-container")[0];
+	if (container.requestFullscreen) {
+		container.requestFullscreen();
+	} else if (container.mozRequestFullScreen) { /* Firefox */
+		container.mozRequestFullScreen();
+	} else if (container.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+		container.webkitRequestFullscreen();
+	} else if (container.msRequestFullscreen) { /* IE/Edge */
+		container.msRequestFullscreen();
+	}
 });
 
 // Optional: Automatic slideshow (for demonstration purposes)
-setInterval(() => {
-	slideIndex++;
-	showSlide(slideIndex);
-}, 5000);
+autoPlay(); // Start the slideshow automatically when the page loads
